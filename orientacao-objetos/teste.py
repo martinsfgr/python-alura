569f21956d70a1a58c11ef6e2c18164cf1776be3
Mauricio <mauriciofagner2010@hotmail.com>
2023-01-17 20:42:33 -0300

    Começar a construir classes

--- orientacao-objetos/teste.py
+++ orientacao-objetos/teste.py
@@ -10,3 +10,11 @@ def saca(conta, valor):
 
 def extrato(conta):
     print("Saldo {}".format(conta["saldo"]))
+
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print(f"Construindo objeto... {self}")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
