b5b853c22d73c60cf5dcd068b875843f36f54528
Mauricio <mauriciofagner2010@hotmail.com>
2023-01-17 21:21:12 -0300

    Remover funções que não são da classe

--- orientacao-objetos/teste.py
+++ orientacao-objetos/teste.py
@@ -1,16 +1,3 @@
-def cria_conta(numero, titular, saldo, limite):
-    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
-    return conta
-
-def deposita(conta, valor):
-    conta["saldo"] += valor
-
-def saca(conta, valor):
-    conta["saldo"] -= valor
-
-def extrato(conta):
-    print("Saldo {}".format(conta["saldo"]))
-
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo objeto... {self}")
