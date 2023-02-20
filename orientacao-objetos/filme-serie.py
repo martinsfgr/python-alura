18356288ee0d637d0f067bedf0fdf6dc53e60416
Mauricio <mauriciofagner2010@hotmail.com>
2023-02-20 20:38:54 -0300

    Reduzir a quantidade de ifs

--- orientacao-objetos/filme-serie.py
+++ orientacao-objetos/filme-serie.py
@@ -24,6 +24,9 @@ class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+    
+    def imprime(self):
+        print(f"Filme: {self._nome}, Ano: {self.ano}, Duração: {self.duracao} minutos, Likes: {self._likes}")
 
 
 class Serie(Programa):
@@ -31,16 +34,18 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def imprime(self):
+        print(f"Série: {self._nome}, Ano: {self.ano}, Temporadas: {self.temporadas}, Likes: {self._likes}")
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}")
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
-print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}")
 
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhe = programa.duracao if hasattr(programa, "duracao") else programa.temporadas
-    print(f"{programa.nome} - {programa.ano} - {detalhe} - {programa.likes}")
+    #detalhe = programa.duracao if hasattr(programa, "duracao") else programa.temporadas
+    #print(f"{programa.nome} - {programa.ano} - {detalhe} - {programa.likes}")
+
+    programa.imprime()
